a4631412e773a686ece82de6bfa4d839e42a0dd4
Meet <meetmunjapara212@gmail.com>
2022-10-11 23:56:15 +0530

    Card Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,14 +13,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode: ThemeMode.dark,
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Hello World'),
+          title: Text('Card'),
         ),
         body: Center(
-          child: Text("My Flutter App"),
+          child: Container(
+            width: 200,
+            height: 200,
+            child: Card(
+              child: Text('Card'),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              color: Colors.green,
+              elevation: 50,
+              shadowColor: Colors.yellow, //border shadow
+            ),
+          ),
         ),
       ),
     );
